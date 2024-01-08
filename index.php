022035c520bf0107df3dbeb69ba1d456a92785ee
Jéssica Pamplona De Almeida <romulopamplona958@gmail.com>
2024-01-07 21:26:56 -0300

    feat: página inicial do projeto

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="index.css">
+        <title>Controle de Vendas</title>
+    </head>
+    <body>
+        <h1>Controle de estoque e venda de produtos</h1>
+        <p>Escolha uma opção:</p>
+        
+        <a href="produtos.php"><button>Dados dos Produtos</button></a>
+        <a href="vendas.php"><button>Vendas</button></a>
+        <a href="balanco.php"><button>Balanço</button></a>
+    </body>
+</html>
