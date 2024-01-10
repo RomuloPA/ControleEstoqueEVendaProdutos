ba76f2935c3a4e14803cf6112cbeaa7f78f651f0
Jéssica Pamplona De Almeida <romulopamplona958@gmail.com>
2024-01-10 18:12:50 -0300

    feat: estrutura html da página das vendas

---
+++ vendas.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="vendas.css">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Cadastro de Vendas</h1>
+
+        <a href=""><button id="adicionar-venda-btn">Adicionar Venda</button></a>
+
+        <a href="index.php"><button id="voltar-btn">Voltar para página inicial</button></a>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Valor Compra</th>
+                    <th>Valor Venda</th>
+                    <th>Dinheiro / PIX</th>
+                    <th>Cartão Débito</th>
+                    <th>Cartão Crédito</th>
+                    <th>Quantidade de Parcelas</th>
+                    <th>Valor Recebido da Maquininha</th>
+                    <th>Lucro</th>
+                    <th>Data da Venda</th>
+                    <th>Cliente</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+        </table>
+    </body>
+</html>
